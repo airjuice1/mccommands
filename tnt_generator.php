cec8b8d6d498621257cd94cf4b1347cd23ded1b8
airjuice1 <airjuice1@gmail.com>
2021-12-27 21:56:15 +0300

    init

--- tnt_generator.php
+++ tnt_generator.php
@@ -1,95 +1,38 @@
 <?php
-const X = 360;
-const Y = 75;
-const Z = 212;
+const X = -24;
+const Y = 0;
+const Z = -51;
 
-$tmp['tnt'] = ['x' => [], 'z' => []];
-$tmp['air'] = ['x' => [], 'z' => []];
+$command['tnt'] = [];
+$command['air'] = [];
 
-$min_x = X - 300;
-$max_x = X + 300;
+$step		 = 50;
+$perimetr    = 550;
+$start_pos_x = X - $perimetr;
+$final_pos_x = X + $perimetr;
+$start_pos_z = Z - $perimetr;
+$final_pos_z = Z + $perimetr;
 
-for ($i = $min_x; $i <= $max_x; $i = $i + 20)
+for ($z = $start_pos_z; $z <= $final_pos_z; $z = $z + $step, $start_pos_z = $start_pos_z + $step) 
 {
-	$x1 = $i + rand(0,2);
-	$y1 = Y;
-	$z1 = Z + rand(1,2);
+	$z1 = $start_pos_z;
 
-	$x2 = $x1 + rand(0,2);
-	$y2 = $y1 + rand(10,90);
-	$z2 = $z1 + rand(0,2);
+	for ($x = $start_pos_x; $x <= $final_pos_x ; $x = $x + $step)
+	{
+		$x1 = $x;
+		$y1 = Y;		
+		$x2 = $x + 1;
+		$y2 = Y + rand(100,200);
+		$z2 = $z1 + 1;		
 
-	$tmp['tnt']['x'][] = "/fill " . $x1 . " " . $y1 . " " . $z1 . " " . $x2 . " " . $y2 . " " . $z2 . " minecraft:tnt\n";
-	$tmp['air']['x'][] = "/fill " . $x1 . " " . $y1 . " " . $z1 . " " . $x2 . " " . $y2 . " " . $z2 . " minecraft:air\n";
-
+		$command['air'][] = '/fill ' . $x1 . ' ' . $y1 . ' ' . $z1 . ' ' . $x2 . ' ' . $y2 . ' ' . $z2 . ' minecraft:air' . "\n";
+		$command['tnt'][] = '/fill ' . $x1 . ' ' . $y1 . ' ' . $z1 . ' ' . $x2 . ' ' . $y2 . ' ' . $z2 . ' minecraft:tnt' . "\n";
+	}
 }
 
 
-
-for ($j = $min_x; $j <= $max_x; $j = $j + 20)
-{
-	$x1 = X + rand(0,2);
-	$y1 = Y;
-	$z1 = $j + rand(1,2);
-
-	$x2 = $x1 + rand(0,2);
-	$y2 = $y1 + rand(10,90);
-	$z2 = $z1 + rand(0,2);
-
-	$tmp['tnt']['z'][] = "/fill " . $x1 . " " . $y1 . " " . $z1 . " " . $x2 . " " . $y2 . " " . $z2 . " minecraft:tnt\n";
-	$tmp['air']['z'][] = "/fill " . $x1 . " " . $y1 . " " . $z1 . " " . $x2 . " " . $y2 . " " . $z2 . " minecraft:air\n";
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-foreach ($tmp['air'] as $key => $value) 
-{		
-	foreach($value as $v) echo $v;
-	foreach($value as $v) echo $v;
-}
-
-echo "\n\n\n\n\n\n";
-
-foreach ($tmp['tnt'] as $value) 
-{
-	foreach($value as $v) echo $v;
-	foreach($value as $v) echo $v;
-}
-
-echo "\n\n\n\n\n\n";
+echo "\n\n\n\n\n";
+foreach ($command['air'] as $key => $value) echo $value;
+echo "\n\n\n\n\n";
+foreach ($command['tnt'] as $key => $value) echo $value;
+echo "\n\n\n\n\n";
